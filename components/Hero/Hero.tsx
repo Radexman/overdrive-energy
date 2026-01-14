c3a27186f1503ef96a91cf985aed6d7bed41bf00
Radosław Siek <radoslaw.siek@hued.me>
2026-01-14 15:20:05 +0100

    hero timeline

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,9 +1,24 @@
 'use client';
 
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
 import { Environment, View } from '@react-three/drei';
+
 import HeroScene from './HeroScene';
 
+gsap.registerPlugin(useGSAP);
+
 const Hero = () => {
+  useGSAP(() => {
+    const introTl = gsap.timeline();
+
+    introTl
+      .set('.hero', { opacity: 1 })
+      .from('.hero-heading', { scale: 2, opacity: 0, ease: 'power4.in', delay: 0.3 })
+      .from('.hero-subheading', { opacity: 0, y: 20 }, '+=0.3')
+      .from('.hero-tagline', { opacity: 0, y: 20 });
+  }, []);
+
   return (
     <div className="h-screen w-full">
       <View className="w-dhw h-dvh">
@@ -11,16 +26,10 @@ const Hero = () => {
         <Environment preset="studio" />
         <ambientLight intensity={0.5} />
       </View>
-      <div className="font-bebas-neue absolute inset-0 flex flex-col items-center justify-center text-white">
-        <h1 id="hero-heading" className="mb-6 text-9xl font-bold tracking-wider">
-          OVERDRIVE
-        </h1>
-        <h2 id="hero-subheading" className="text-5xl font-semibold tracking-wide">
-          ENERGY DRINK
-        </h2>
-        <p id="hero-tagline" className="text-3xl font-medium text-yellow-400">
-          Push Beyond Limits
-        </p>
+      <div className="hero font-bebas-neue absolute inset-0 flex flex-col items-center justify-center text-white opacity-0">
+        <h1 className="hero-heading mb-6 text-9xl font-bold tracking-wider">OVERDRIVE</h1>
+        <h2 className="hero-subheading text-5xl font-semibold tracking-wide">ENERGY DRINK</h2>
+        <p className="hero-tagline text-3xl font-medium text-yellow-400">Push Beyond Limits</p>
       </div>
     </div>
   );
--- components/Hero/HeroScene.tsx
+++ components/Hero/HeroScene.tsx
@@ -1,14 +1,37 @@
 'use client';
 
+import * as THREE from 'three';
+import gsap from 'gsap';
+import { useRef } from 'react';
+import { useGSAP } from '@gsap/react';
+import { ScrollTrigger } from 'gsap/all';
+
 import Can from '../Can';
 
+gsap.registerPlugin(useGSAP, ScrollTrigger);
+
 const HeroScene = () => {
+  const canOneRef = useRef<THREE.Group>(null);
+  const canTwoRef = useRef<THREE.Group>(null);
+
+  useGSAP(() => {
+    if (!canOneRef.current || !canTwoRef.current) return;
+
+    gsap.set(canOneRef.current.position, { x: -10 });
+    gsap.set(canTwoRef.current.position, { x: 10 });
+
+    const introTl = gsap
+      .timeline({ defaults: { duration: 2, ease: 'back.out(1.4)' } })
+      .to(canOneRef.current.position, { x: -4 })
+      .to(canTwoRef.current.position, { x: 4 }, '<');
+  }, []);
+
   return (
     <group>
-      <group position-x={-4}>
+      <group ref={canOneRef} position-x={-4}>
         <Can textureName="lemon-lime" />
       </group>
-      <group position-x={4}>
+      <group ref={canTwoRef} position-x={4}>
         <Can textureName="orange-burst" />
       </group>
     </group>
